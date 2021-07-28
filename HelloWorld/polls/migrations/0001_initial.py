199214ac75fc2afb7d4daa516c9bee728803ad7b
devil-prog <krishmikaabisheka1@gmail.com>
2021-07-28 19:21:01 +0530

    Enough for today

--- HelloWorld/polls/migrations/0001_initial.py
+++ HelloWorld/polls/migrations/0001_initial.py
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
             name='Choice',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('choice_text', models.IntegerField(default=0)),
+                ('choice_text', models.CharField(max_length=200)),
                 ('votes', models.IntegerField(default=0)),
                 ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.question')),
             ],
